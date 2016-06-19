39a62a25c775bac668e942c58f1ee7e7da94055a
marcelo siedler <siedler@gmail.com>
2016-06-19 10:38:06 -0300

    Transição da cena inicial
    Troca de cena sem clique no ifsul

--- Assets/Scripts/sceneSplash.cs
+++ Assets/Scripts/sceneSplash.cs
@@ -5,4 +5,19 @@ using UnityEngine.SceneManagement;
 public class sceneSplash : MonoBehaviour {
 
 
+	float time;
+	void Start(){
+		time = 3;
+	}
+
+	void Update(){
+		if (time < 0) {
+			comandosBasicos c = new comandosBasicos ();
+			c.carregaCena ("Home");
+		} else {
+			time -= Time.deltaTime;
+		}
+	
+	}
+
 }
